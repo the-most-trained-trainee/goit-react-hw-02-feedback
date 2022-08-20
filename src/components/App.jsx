8706bae6113e0c491a33c913271a1fb289cbfbe8
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-20 12:34:28 +0300

    Interim Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,33 @@
+import React from 'react';
 import FeedBackStats from './FeedBackStats';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <FeedBackStats
-        good={0}
-        neutral={0}
-        bad={0}
-        total={0}
-        positivePercentage={0}
-      />
-    </div>
-  );
-};
+class App extends React.Component {
+
+
+
+  
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <FeedBackStats
+          good={0}
+          neutral={0}
+          bad={0}
+          total={0}
+          positivePercentage={0}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
