e9f4ecf38a5df48352e30f556ea8ab0f2f92b861
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-12-10 19:45:47 +0200

    Shortcomings exclusion 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Section from './Section';
-import Statistics from './Statistics';
-import FeedbackOptions from './FeedbackOptions';
-import Notification from './Notification';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './Options/FeedbackOptions';
+import Notification from './Notification/Notification';
 
 class App extends React.Component {
   state = {
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 
 const Notification = ({ message }) => <p>{message}</p>;
 
-export default Notification;
-
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
 };
+
+export default Notification;
